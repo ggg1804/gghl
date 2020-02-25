026799a1491b3a4fec59103af6765122c875f485
Ggg1804 <baek.peach04@gmail.com>
2020-02-25 12:05:49 +0300

    class 2

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -9,5 +9,20 @@ namespace ConsoleApp1
         public string Name { get; set; }
         public string Ciphen { get; set; }
         public int Quantity { get; set; }
+        public void ShowInforamation()
+        {
+            Console.WriteLine("Название - {0}", Name);
+            Console.WriteLine("Шифр - {0}", Ciphen);
+            Console.WriteLine("количество - {0}", Quantity);
+        }
+        public void CreateInformation()
+        {
+            Console.WriteLine("Введите название: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите шифр: }");
+            Ciphen = Console.ReadLine();
+            Console.WriteLine("Введите шифр: ");
+            Quantity = Convert.ToInt32(Console.ReadLine());
+        }
     }
 }
