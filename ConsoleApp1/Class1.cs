80bee675c47d659bc931e6e7eccbab79acdc3ea3
Ggg1804 <baek.peach04@gmail.com>
2020-02-25 11:57:28 +0300

    Class

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public string Name { get; set; }
+        public string Ciphen { get; set; }
+        public int Quantity { get; set; }
+    }
+}
